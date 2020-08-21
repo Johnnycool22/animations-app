163d742ac360086f87fd667ce1faf78bf153031a
John <jkforsythe@hotmail.com>
2020-08-21 23:16:53 +0100

    Transition wrapping added to modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -30,17 +31,36 @@ class App extends Component {
           Toggle
         </button>
         <br />
-        {this.state.showBlock ? (
-          <div 
-            style={{
-              backgroundColor: 'red',
-              width: 100,
-              height: 100,
-              margin: 'auto'
-            }}>
-          </div>) : null}
-        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
-        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
+        <Transition 
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit>
+            {state => (
+                <div 
+                  style={{
+                    backgroundColor: 'red',
+                    width: 100,
+                    height: 100,
+                    margin: 'auto',
+                    transition: 'opacity 1s ease-out',
+                    opacity: state === 'exiting' ? 0 : 1
+                }}>
+              </div>
+            )}
+        </Transition>
+        <Transition
+        mountOnEnter
+        unmountOnExit 
+          in={this.state.modalIsOpen} 
+          timeout={300}>
+            {state => (
+              <Modal show={state} closed={this.closeModal} /> 
+            )}
+        </Transition>
+        {this.state.modalIsOpen ? (
+          <Backdrop show />
+         ) : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
